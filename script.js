004c4856069e724d428ef44ad0d798a2bab36cc2
mohamedoulahguine <mohamedoulahguine@gmail.com>
2025-04-18 11:08:11 +0300

    working with some js code

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 "use strict";
-x = 10;
-console.log(x);
+
+let font_color = document.querySelectorAll(".font_color ul li");
+let font_size = document.querySelectorAll(".font_size ul li");
+
+font_color.forEach((ele) => {
+  ele.addEventListener("click", function () {
+    font_color.forEach((el) => el.classList.remove("active"));
+    ele.classList.toggle("active");
+  });
+});
+
+font_size.forEach((ele) => {
+  ele.addEventListener("click", function () {
+    font_size.forEach((el) => el.classList.remove("active"));
+    ele.classList.toggle("active");
+  });
+});
